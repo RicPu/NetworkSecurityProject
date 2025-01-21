12cbb58b5555e34ab25c359ba745fe29e78c01a8
RicPu <144445283+RicPu@users.noreply.github.com>
2025-01-21 22:15:01 +0100

    client can request data

--- code/src/QUIC_client.py
+++ code/src/QUIC_client.py
@@ -3,10 +3,10 @@ import ssl
 import json
 import asyncio
 import logging
-from aioquic.quic.events import QuicEvent, HandshakeCompleted, StreamDataReceived, ConnectionTerminated
+from aioquic.asyncio.client import connect
 from aioquic.quic.configuration import QuicConfiguration
 from aioquic.asyncio.protocol import QuicConnectionProtocol
-from aioquic.asyncio.client import connect
+from aioquic.quic.events import QuicEvent, HandshakeCompleted, StreamDataReceived
 
 
 logging.basicConfig(level=logging.INFO)
@@ -15,74 +15,88 @@ logging.basicConfig(level=logging.INFO)
 class FileTransferClient(QuicConnectionProtocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.connected_event = asyncio.Event()
+        self.stream_data = {}
         self.logger = logging.getLogger(__name__)
 
-    # For debugging purposes
     def quic_event_received(self, event: QuicEvent):
         if isinstance(event, HandshakeCompleted):
             self.logger.info("Handshake completed!")
-            self.connected_event.set()
-        elif isinstance(event, StreamDataReceived):
-            self.logger.info(f"Stream data received: {len(event.data)} bytes on stream {event.stream_id}")
-        elif isinstance(event, ConnectionTerminated):
-            self.logger.info(f"Connection terminated: {event.error_code}, reason: {event.frame_type}")
-    
-    def close(self):
-        self.logger.info("Closing connection.")
-        super().close()
+        if isinstance(event, StreamDataReceived):
+            stream_id = event.stream_id
+            data = event.data
+
+            if stream_id not in self.stream_data:
+                metadata, remaining_data = self.parse_metadata(data)
+
+                file_name = metadata.get("file_name")
+                file_size = metadata.get("file_size")
+
+                self.stream_data[stream_id] = {
+                    "data": bytearray(remaining_data),
+                    "file_name": file_name,
+                    "file_size": file_size,
+                }
+            else:
+                self.stream_data[stream_id]["data"].extend(data)
+
+            if event.end_stream:
+                file_name = self.stream_data[stream_id]["file_name"]
+                self.save_file(file_name, self.stream_data[stream_id]["data"])
+                self.stream_data.pop(stream_id, None)
+
+    def parse_metadata(self, data: bytes):
+        try:
+            metadata, remaining_data = data.split(b"\n", 1)
+            return json.loads(metadata.decode()), remaining_data
+
+        except Exception as e:
+            self.logger.error(f"Error parsing metadata: {e}")
+            return None, data
+
+    def save_file(self, file_name: str, file_data: bytes):
+        try:
+            save_path = os.path.join("code/assets/client_directory", file_name)
+            os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+            with open(save_path, "wb") as file:
+                file.write(file_data)
+            self.logger.info(f"File saved as '{save_path}'")
+
+        except Exception as e:
+            self.logger.error(f"Error saving file '{file_name}': {e}")
 
 
-async def send_file(host: str, port: int, file_path: str, configuration: QuicConfiguration):
+async def send_request(client: QuicConnectionProtocol, request: bytes):
+    stream_id = client._quic.get_next_available_stream_id(is_unidirectional=True)
+    reader, writer = await client.create_stream(stream_id)
+
+    try:
+        writer.write(request)
+        await writer.drain()
+        writer.write_eof()
+        await writer.drain()
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
+async def main(host: str, port: int, configuration: QuicConfiguration, action: str):
     async with connect(
         host, port, configuration=configuration, create_protocol=FileTransferClient
-    ) as protocol:
-        
-        await protocol.wait_connected()
+    ) as client:
+        await client.wait_connected()
 
-        file_name = file_path.split("/")[-1]
-        file_size = os.path.getsize(file_path)
-
-        metadata = json.dumps({
-            "file_name": file_name,
-            "file_size": file_size
-        }).encode()
-
-        stream_id = protocol._quic.get_next_available_stream_id(is_unidirectional=False)
-        reader, writer = await protocol.create_stream(stream_id)
-
-        try:
-            writer.write(metadata + b'\n')
-            await writer.drain()
-
-            with open(file_path, "rb") as f:
-                while chunk := f.read(8192):
-                    writer.write(chunk)
-                    await writer.drain()
-            logging.info(f"File {file_path} sent successfully.")
-
-        finally:
-            writer.close()
-            # I don't get why writer stays open after drain.
-            # The connection will never end if it doesn't close, so I'm
-            # forcing the closing after 5 seconds
-            try:
-                await asyncio.wait_for(writer.wait_closed(), timeout=5)
-            
-            except asyncio.TimeoutError:
-                logging.warning("Writer wait_closed timed out. Forcing cleanup.")
-            
-        protocol.close()
-        await protocol.wait_closed()
-        logging.info("QUIC connection closed.")
+        request = json.dumps(
+            {"action": "request_file", "file_name": "Summer_1.jpg"}
+        ).encode()
+        await send_request(client, request)
 
 
 if __name__ == "__main__":
     host = "localhost"
     port = 4433
-    image_path = "code/assets/Summer_1.jpg"
 
     configuration = QuicConfiguration(is_client=True)
     configuration.verify_mode = ssl.CERT_NONE
 
-    asyncio.run(send_file(host, port, image_path, configuration))
+    asyncio.run(main(host, port, configuration, None))
--- code/src/QUIC_server.py
+++ code/src/QUIC_server.py
@@ -1,86 +1,83 @@
 import os
 import json
-import asyncio
 import logging
-from aioquic.quic.events import StreamDataReceived, HandshakeCompleted, ConnectionTerminated
+import asyncio
+from aioquic.asyncio.server import serve
+from aioquic.quic.events import QuicEvent, HandshakeCompleted, StreamDataReceived
 from aioquic.quic.configuration import QuicConfiguration
 from aioquic.asyncio.protocol import QuicConnectionProtocol
-from aioquic.asyncio.server import serve
+
 from utils import gen_key_cert
 
 
 logging.basicConfig(level=logging.INFO)
 
 
-class FileTransferProtocol(QuicConnectionProtocol):
+class FileTransferServer(QuicConnectionProtocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stream_data = {}
-        self.connected_event = asyncio.Event()
         self.logger = logging.getLogger(__name__)
 
-    def parse_metadata(self, data):
-        try:
-            metadata, remaining_data = data.split(b'\n', 1)
-            return json.loads(metadata.decode()), remaining_data
-        
-        except Exception as e:
-            self.logger.error(f"Error parsing metadata: {e}")
-            return None, data
-
-    def save_file(self, file_name, file_data):
-        try:
-            os.makedirs("received_files", exist_ok=True)
-            output_path = os.path.join("received_files", file_name)
-
-            with open(output_path, "wb") as file:
-                file.write(file_data)
-            self.logger.info(f"File saved as '{output_path}'")
-
-        except Exception as e:
-            self.logger.error(f"Error saving file '{file_name}': {e}")
-
-    def quic_event_received(self, event):
+    def quic_event_received(self, event: QuicEvent):
         if isinstance(event, HandshakeCompleted):
             self.logger.info("Handshake completed!")
-            self.connected_event.set()
-
         elif isinstance(event, StreamDataReceived):
             stream_id = event.stream_id
             data = event.data
 
             if stream_id not in self.stream_data:
-                metadata, remaining_data = self.parse_metadata(data)
-                if metadata:
-                    file_name = metadata.get("file_name", f"received_file_{stream_id}")
-                    self.stream_data[stream_id] = {
-                        "data": bytearray(remaining_data),
-                        "file_name": file_name
-                    }
+                request = json.loads(data.decode())
+                action = request.get("action")
+
+                if action == "request_file":
+                    file_name = request.get("file_name")
+                    asyncio.create_task(self.request_file(file_name))
                 else:
-                    self.logger.warning("Metadata missing. Using default filename.")
-                    self.stream_data[stream_id] = {
-                        "data": bytearray(data),
-                        "file_name": f"received_file_{stream_id}"
-                    }
-            
+                    self.logger.error("Unknown action received from client.")
+
             else:
                 self.stream_data[stream_id]["data"].extend(data)
 
             if event.end_stream:
-                file_name = self.stream_data[stream_id]["file_name"]
-                self.save_file(file_name, self.stream_data[stream_id]["data"])
                 self.stream_data.pop(stream_id, None)
 
-        elif isinstance(event, ConnectionTerminated):
-            self.logger.info(f"Connection terminated: {event.error_code}, reason: {event.frame_type}")
+    async def request_file(self, file_name: str):
+        self.logger.info(f"Client is requesting file: {file_name}")
+        file_path = os.path.join("code/assets/server_directory", file_name)
+
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            stream_id = self._quic.get_next_available_stream_id(is_unidirectional=True)
+            reader, writer = await self.create_stream(stream_id)
+
+            file_size = os.path.getsize(file_path)
+            metadata = json.dumps(
+                {"file_name": file_name, "file_size": file_size}
+            ).encode()
+
+            try:
+                writer.write(metadata + b"\n")
+                await writer.drain()
+
+                with open(file_path, "rb") as file:
+                    while chunk := file.read(4096):
+                        writer.write(chunk)
+                        await writer.drain()
+                    writer.write_eof()
+                    await writer.drain()
+
+                self.logger.info("File sent.")
+
+            finally:
+                writer.close()
+                await writer.wait_closed()
 
 
 async def main(host: str, port: int, configuration: QuicConfiguration):
     await serve(
-        host, port, configuration=configuration,
-        create_protocol=FileTransferProtocol
+        host, port, configuration=configuration, create_protocol=FileTransferServer
     )
+
     logging.info("Server running...")
     await asyncio.Future()
 
@@ -88,12 +85,11 @@ async def main(host: str, port: int, configuration: QuicConfiguration):
 if __name__ == "__main__":
     host = "::"
     port = 4433
-    gen_key_cert() # Generate private key and certificate
+    gen_key_cert()
 
     configuration = QuicConfiguration(is_client=False)
     configuration.load_cert_chain(
-        certfile="code/assets/certificate.pem", 
-        keyfile="code/assets/private_key.pem"
+        certfile="code/assets/certificate.pem", keyfile="code/assets/private_key.pem"
     )
 
     asyncio.run(main(host, port, configuration))
